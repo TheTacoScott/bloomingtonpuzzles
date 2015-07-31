ec620f1b70eb539e37b94975e20136b4147e7a95
Scott Powers <scott.powers@untsystem.edu>
2015-07-31 16:25:08 -0500

    first round of pylint cleanup

--- wine.py
+++ wine.py
@@ -1,3 +1,10 @@
+"""
+wine.py
+a bipartite non directed graphing solution to the bloomreach wine puzzle
+example usage:
+  python wine.py --input filename.txt
+"""
+
 import networkx as nx
 import argparse, time
 
@@ -20,20 +27,23 @@ g_wine_node_count = 0
 g_root_node_count = 1
 
 def add_line_to_graph(line):
-  global g_person_node_count,g_wine_node_count
-  (person,wine) = line[0:-1].split("\t")
-  person = person.replace("person","p")
-  wine = wine.replace("wine","w")
+  """
+  helper function to parse and add a line to the graph
+  """
+  global g_person_node_count, g_wine_node_count
+  (person, wine) = line[0:-1].split("\t")
+  person = person.replace("person", "p")
+  wine = wine.replace("wine", "w")
   if not person in fg:
-    fg.add_node(person,{"c": 0})
+    fg.add_node(person, {"c": 0})
     g_person_node_count += 1
   if not wine in fg:
     fg.add_node(wine)
     g_wine_node_count += 1
-  fg.add_edge(person,wine)
-  fg.add_edge(person,"r")
+  fg.add_edge(person, wine)
+  fg.add_edge(person, "r")
 
-f = open(args.input,"r")
+f = open(args.input, "r")
 valid_line = True
 
 #PROCESS NODES
@@ -65,9 +75,9 @@ while nodes_to_process:
   wine_node_with_fewest_edges = None
   wine_node_with_fewest_edges_edge_count = FEWER_COMPARE
   wine_search_count = 0
-  for node in nx.dfs_postorder_nodes(fg,"r"): #dfs postorder is magic and should be worshiped. --Andy Weir
+  for node in nx.dfs_postorder_nodes(fg, "r"): #dfs postorder is magic and should be worshiped. --Andy Weir
     if node == "r": continue #skip root node
-    if node[0]=="p": continue #doubt we'll ever hit here, but skip people nodes
+    if node[0] == "p": continue #doubt we'll ever hit here, but skip people nodes
 
     wine_neighbors = fg.neighbors(node)
     wine_neighbor_count = len(wine_neighbors)
@@ -79,13 +89,13 @@ while nodes_to_process:
       if wine_neighbor_count <= lowest_wine_edge_count: break
 
   if wine_search_count > 25: #optimization that is unlikley to be needed with dfs postorder and these data sets
-    lowest_wine_edge_count = min(lowest_wine_edge_count + 1,10)
+    lowest_wine_edge_count = min(lowest_wine_edge_count + 1, 10)
   else:
     lowest_wine_edge_count = wine_node_with_fewest_edges_edge_count
   # END WINE SECTION
 
   # we're out of edges, time to call it a day
-  if not wine_node_with_fewest_edges: 
+  if not wine_node_with_fewest_edges:
     nodes_to_process = False
     break
 
@@ -107,11 +117,16 @@ while nodes_to_process:
   if person_node_with_fewest_edges and wine_node_with_fewest_edges:
     fg.node[person_node_with_fewest_edges]["c"] += 1
     wine_sold += 1
-    print "{2: >10}\t{0: >10}\t{1: >10}\tBuffer: {3}\tW: {4}\tP:{5}".format(person_node_with_fewest_edges,wine_node_with_fewest_edges,wine_sold,g_person_node_count+g_wine_node_count,g_wine_node_count,g_person_node_count)
+    print "{2: >10}\t{0: >10}\t{1: >10}\tBuffer: {3}\tW: {4}\tP:{5}".format(person_node_with_fewest_edges,
+                                                                            wine_node_with_fewest_edges,
+                                                                            wine_sold,
+                                                                            g_person_node_count+g_wine_node_count,
+                                                                            g_wine_node_count,
+                                                                            g_person_node_count)
     if fg.node[person_node_with_fewest_edges]["c"] == MAX_WINE:
       fg.remove_node(person_node_with_fewest_edges)
-      g_person_node_count -= 1 
+      g_person_node_count -= 1
     fg.remove_node(wine_node_with_fewest_edges)
     g_wine_node_count -= 1
 f.close()
-print args.min_buffer_size,args.max_buffer_size,wine_sold,round(time.time()-start,3)
+print args.min_buffer_size, args.max_buffer_size, wine_sold, round(time.time()-start, 3)
