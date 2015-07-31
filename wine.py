a2dd5df52a2d12e0eb7ecf88af8b4084c23a7426
Scott Powers <scott.powers@untsystem.edu>
2015-07-31 10:24:31 -0500

    started to implement the concept of a rolling buffer of nodes such that a data set of any sized can be processed with mapreduce (we'll miss edges but so would mapreduce is my understanding) removed todos

--- wine.py
+++ wine.py
@@ -5,32 +5,38 @@ parser = argparse.ArgumentParser(description='')
 parser.add_argument('--input', action="store", dest="input")
 args= parser.parse_args()
 MAX_WINE = 3
+MAX_MEM_NODE_COUNT = 500000
 
 fg = nx.Graph() #final graph
 ng = nx.Graph() #neighbor count graph
 
+g_person_node_count = 0
+g_wine_node_count = 0
+g_root_node_count = 1
+
 f = open(args.input,"r")
 for line in f:
-  (person,wine) = line[0:-1].split("\t")
-  person = person.replace("person","p")
-  wine = wine.replace("wine","w")
-  fg.add_node(person,{"c": 0})
-  fg.add_node(wine)
-    
-  fg.add_edge(person,wine)
-  fg.add_edge(person,"r")
+  while True:
+    if (g_person_node_count+g_wine_node_count) < MAX_MEM_NODE_COUNT:
+      (person,wine) = line[0:-1].split("\t")
+      person = person.replace("person","p")
+      wine = wine.replace("wine","w")
+      if not person in fg:
+        fg.add_node(person,{"c": 0})
+        g_person_node_count += 1
+      if not wine in fg:
+        fg.add_node(wine)
+        g_wine_node_count += 1
+      fg.add_edge(person,wine)
+      fg.add_edge(person,"r")
+    else:
+      pass
+      #process here
+      
   
 f.close()
 
-#TODO: test less graph-y approach, but I bet it will be unusuable as scale increases, and this is clearly a graphing problem
-#TODO: need to think of this in terms of map/combine/reduce (big data yo)
-#TODO: output is out of order, easy fix if using mapreduce method, will have to output file, then change first line later... which stinks (for now)
-#TODO: need to look into a method to iterate thought a sorted version of the nodes in order of edge count, not sure how to do this yet.
-#TODO: use an additional node with numeral name of how many edges the nodes under it have, use tree traversal to navigate down it pre/post
-#TODO: break up data and make small graphs. bucket persons based on 1st 2 digits of id or some such. we'll lose edges, but the speed gain will be significant (similar in mapreduce)
-#TODO: write my own distributed graph processing daemons to handle problems like this
-#TODO: possibly have a rolling adjustment of wine node edge counts the processor will accept. start with 1, then if you have to traverse too far down the tree to find another 1, start accepting 2, etc.
-#TODO: since we're a bipartite/non directed graph, we could use a maximal matching algorithm. (thanks old comp sci textbook for reminding me of names)
+sys.exit()
 
 wine_sold = 0
 lowest_wine_edge_count = 1
