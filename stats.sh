fe74158c6bc940d4207f0fc1d22bf7df95aeaf57
Scott Powers <scott.powers@untsystem.edu>
2015-07-31 16:14:00 -0500

    changed stats tool to generate cleaner output

--- stats.sh
+++ stats.sh
@@ -5,8 +5,7 @@ then
   do 
     for max in `seq $min 100000 1000000`
     do 
-      echo "$min $max"
-      python wine.py --input "$1" --min-buffer=$min --max-buffer=$max 
+      python wine.py --input "$1" --min-buffer=$min --max-buffer=$max | tail -n1
       sleep 0.01
     done 
   done
