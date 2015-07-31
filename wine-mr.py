a7b445d9e6b0eea02ed977ac707595e5084b2ebc
Scott <withorwithoutgod@gmail.com>
2015-07-30 21:41:22 -0500

    not loving the mr solution yet, the max of 3 wine is going to be trickey with mr

--- wine-mr.py
+++ wine-mr.py
@@ -5,29 +5,42 @@ class WineFind(MRJob):
 
     def steps(self):
       return [
-        MRStep(mapper=self.edge_mapper,reducer=self.edge_reducer)
+        MRStep(mapper=self.edge_mapper,reducer=self.edge_reducer),
+        #MRStep(mapper=self.sell_mapper),#,reducer=self.sell_reducer),
       ]
     def edge_mapper(self, _, line):
-        person = line.split("\t")[0]
-        wine = line.split("\t")[1]
+        person = line.split("\t")[0].replace("person","p")
+        wine = line.split("\t")[1].replace("wine","w")
         yield "edges",wine + "-" + person
 
     def edge_reducer(self, key, values):
         w = {}
         p = {}
+
         for edge in values:
           (wine,person) = edge.split("-")
-          if person not in p: p[person] = []
-          if wine not in w:   w[wine] = []
 
-          if person not in w[wine]: w[wine].append(person)
-          if wine not in p[person]: p[person].append(wine)
+          if wine not in w:
+            w[wine] = []
+          if person not in p:
+            p[person] = []
 
-        for wine in w:
-          yield wine, "|".join(w[wine])
-        for person in p:
-          yield person, "|".join(p[person])
+          if person not in w[wine]:
+            w[wine].append(person)
+          if wine not in p[person]:
+            p[person].append(wine)
 
+        
+        for cwine in w:
+          p_with_fewest_edges = None
+          p_with_fewest_edges_count = 99999
+          for cperson in w[cwine]:
+            p_edge_count = len(p[cperson])
+            if p_edge_count < p_with_fewest_edges_count:
+              p_with_fewest_edges = cperson
+              p_with_fewest_edges_count = p_edge_count                
+          
+          yield p_with_fewest_edges,cwine
 
 
 if __name__ == '__main__':
