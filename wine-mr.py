b20cbc0bcd10c076306c6e44632403595b1d11b3
Scott <withorwithoutgod@gmail.com>
2015-07-30 20:51:46 -0500

    round 2 of mr wine job. does a good job of connecting neighbors in a way that won't break in hadoop (that I'm aware of)

--- wine-mr.py
+++ wine-mr.py
@@ -6,10 +6,18 @@ class WineFind(MRJob):
     def mapper(self, _, line):
         person = line.split("\t")[0]
         wine = line.split("\t")[1]
-        yield "edge",wine + "-" + person
+        yield "edges",wine + "-" + person
 
     def reducer(self, key, values):
-        yield key, "|".join(values)
+        e = {}
+        for edge in values:
+          (wine,person) = edge.split("-")
+          if wine not in e:
+            e[wine] = []
+          if person not in e[wine]:
+            e[wine].append(person)
+        for wine in e:
+          yield "wine", "|".join(e[wine])
 
 
 if __name__ == '__main__':
