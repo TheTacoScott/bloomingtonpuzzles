416ad06f417edc81b22bb670a2d70f07a8eb88ad
Scott <withorwithoutgod@gmail.com>
2015-08-01 22:44:12 -0500

    got ride of unneeded file open

--- scrambled.py
+++ scrambled.py
@@ -1,16 +1,22 @@
 import itertools
 from pattern.en import parse
+import nltk
 import argparse
+
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--input', action="store", dest="input")
 args = parser.parse_args()
-f = open(args.input, "r")
 with open(args.input, "r") as f:
   for line in f:
     print ""
     rating = 0
     split = line.split(" ")
     
+    print "PATTERN"
     for i in itertools.permutations(split, len(split)): 
       print parse(" ".join(i)).split()
+
+    print "NLTK"
+    for i in itertools.permutations(split, len(split)):
+      print nltk.pos_tag(nltk.word_tokenize(" ".join(i)))
